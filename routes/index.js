c3ebce759d111fc61b6e367f6ca800001e298254
looper93 <rocland@roclanddeMacBook-Pro.local>
2017-03-05 14:54:55 -0800

    for elb settings

--- routes/index.js
+++ routes/index.js
@@ -2,75 +2,12 @@ var express = require('express');
 var router = express.Router();
 var pool = require('./db');
 
-function IsNum(s) {
-    if (s != null && s != "") {
-      return !isNaN(s);
-    }
-    return false;
-}
+router.get('/', function(req, res, next) {
+    res.json({
+        message : 'For ELB settings.'
+    });
+});
 
-router.post('/add', function(req, res, next) {
-    var num1 = req.body.num1;
-    var num2 = req.body.num2;
-    if (!req.session.logged) {
-        res.json({
-            message: 'You are not currently logged in'
-        });
-    }
-    else if (IsNum(num1) && IsNum(num2)) {
-        res.json({
-            message: 'The action was successful',
-            result: num1 + num2
-        });
-    }
-    else {
-        res.json({
-            message:'The numbers you entered are not valid'
-        });
-    }
-});
-router.post('/divide', function(req, res, next) {
-    var num1 = req.body.num1;
-    var num2 = req.body.num2;
-    console.log(num1);
-    console.log(num2);
-    if (!req.session.logged) {
-        res.json({
-            message: 'You are not currently logged in'
-        });
-    }
-    else if (IsNum(num1) && IsNum(num2) && num2 != 0) {
-        res.json({
-            message: 'The action was successful',
-            result: num1 / num2
-        });
-    }
-    else {
-        res.json({
-            message:'The numbers you entered are not valid'
-        });
-    }
-});
-router.post('/multiply', function(req, res, next) {
-    var num1 = req.body.num1;
-    var num2 = req.body.num2;
-    if (!req.session.logged) {
-        res.json({
-            message: 'You are not currently logged in'
-        });
-    }
-    else if (IsNum(num1) && IsNum(num2)) {
-        res.json({
-            message: 'The action was successful',
-            result: num1 * num2
-        });
-    }
-    else {
-        res.json({
-            message:'The numbers you entered are not valid'
-        });
-    }
-});
 router.post('/login', function(req, res, next) {
     var name = req.body.username;
     var password = req.body.password;
